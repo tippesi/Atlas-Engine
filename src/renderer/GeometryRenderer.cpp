eff1ae807bfd219232b71311712c3f0a75cf2e33
Tippex97 <tippex97@gmail.com>
2018-12-04 00:08:12 +0100

    Deleted old rendering code

--- src/renderer/GeometryRenderer.cpp
+++ src/renderer/GeometryRenderer.cpp
@@ -90,67 +90,6 @@ void GeometryRenderer::Render(Window* window, RenderTarget* target, Camera* came
 
 	}
 
-	/*
-	for (auto& shaderConfigBatch : shaderBatch->configBatches) {
-
-		shaderBatch->Bind(shaderConfigBatch->ID);
-
-		diffuseMapUniform->SetValue(0);
-		normalMapUniform->SetValue(1);
-		specularMapUniform->SetValue(2);
-		heightMapUniform->SetValue(3);
-
-		viewMatrixUniform->SetValue(camera->viewMatrix);
-		projectionMatrixUniform->SetValue(camera->projectionMatrix);
-
-		for (auto actorBatch : scene->actorBatches) {
-
-			Mesh* mesh = actorBatch->GetMesh();
-			mesh->Bind();
-
-			for (auto subData : mesh->data->subData) {
-
-				Material* material = mesh->data->materials[subData->materialIndex];
-
-				if (material->geometryConfig->batchID != shaderConfigBatch->ID) {
-					continue;
-				}
-
-				if (material->HasDiffuseMap())
-					material->diffuseMap->Bind(GL_TEXTURE0);
-				if (material->HasNormalMap())
-					material->normalMap->Bind(GL_TEXTURE1);
-				if (material->HasSpecularMap())
-					material->specularMap->Bind(GL_TEXTURE2);
-				if (material->HasDisplacementMap())
-					material->displacementMap->Bind(GL_TEXTURE3);
-
-				diffuseColorUniform->SetValue(material->diffuseColor);
-				specularColorUniform->SetValue(material->specularColor);
-				ambientColorUniform->SetValue(material->ambientColor);
-				specularHardnessUniform->SetValue(material->specularHardness);
-				specularIntensityUniform->SetValue(material->specularIntensity);
-
-				for (auto actor : actorBatch->actors) {
-
-					if (!actor->visible) {
-						continue;
-					}
-
-					modelMatrixUniform->SetValue(actor->transformedMatrix);
-
-					glDrawElements(mesh->data->primitiveType, subData->numIndices, mesh->data->indices->GetType(),
-						(void*)(subData->indicesOffset * mesh->data->indices->GetElementSize()));
-
-				}
-
-			}
-
-		}
-
-	}
-	*/
-
 }
 
 void GeometryRenderer::InitShaderBatch() {
