07a98b67e498d4e7eed54b71ab42d1997e7b060c
Tippex97 <tippex97@gmail.com>
2018-12-10 21:20:44 +0100

    Keyboard events do work now

--- src/events/EngineKeyboardEvent.h
+++ src/events/EngineKeyboardEvent.h
@@ -4,6 +4,7 @@
 #include "../System.h"
 #include "../libraries/SDL/include/SDL.h"
 #include "EventDelegate.h"
+#include "EngineKeycodes.h"
 
 /**
  * A class to distribute keyboard events.
@@ -13,13 +14,17 @@ class EngineKeyboardEvent {
 public:
 	EngineKeyboardEvent(SDL_KeyboardEvent event) {
 
-
+		windowID = event.windowID;
+		keycode = event.keysym.sym;
+		state = event.state;
+		repeat = event.repeat > 0 ? true : false;
 
 	}
 
 	uint32_t windowID;
-	bool keyDown;
-	bool keyRepeat;
+	Keycode keycode;
+	uint8_t state;
+	bool repeat;
 
 };
 
