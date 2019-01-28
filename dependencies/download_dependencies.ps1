5d9a5b582954f2ccc16e0e5e7013a5e744a9d22d
Simon Tippe <tippex97@gmail.com>
2019-01-28 01:54:16 +0100

    Added download scripts

---
+++ dependencies/download_dependencies.ps1
@@ -0,0 +1,41 @@
+add-type @"
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy : ICertificatePolicy {
+    public bool CheckValidationResult(
+        ServicePoint srvPoint, X509Certificate certificate,
+        WebRequest request, int certificateProblem) {
+        return true;
+    }
+}
+"@
+$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
+$file = "SDL.zip"
+
+Invoke-WebRequest -Uri "https://www.libsdl.org/release/SDL2-2.0.9.zip" -OutFile $file -TimeoutSec 5
+
+# Unzip the file to specified location
+mkdir "SDL"
+$location = Get-Location
+$zip_file = (new-object -com shell.application).namespace("$location\$file") 
+$destination = (new-object -com shell.application).namespace("$location\SDL")
+$items = $zip_file.items()
+$destination.Copyhere($items)
+
+$file = "Assimp.zip"
+
+Invoke-WebRequest -Uri "https://www.github.com/assimp/assimp/archive/v4.1.0.zip" -OutFile $file -TimeoutSec 5
+
+# Unzip the file to specified location
+mkdir "Assimp"
+$location = Get-Location
+$zip_file = (new-object -com shell.application).namespace("$location\$file") 
+$destination = (new-object -com shell.application).namespace("$location\Assimp")
+$items = $zip_file.items()
+$destination.Copyhere($items)
+
+Remove-Item SDL.zip
+Remove-Item Assimp.zip
---
+++ dependencies/download_dependencies.sh
@@ -0,0 +1,9 @@
+# Download SDL2 version 2.0.9
+wget https://www.libsdl.org/release/SDL2-2.0.9.zip
+unzip SDL2-2.0.9.zip -d SDL2-2
+rm SDL2-2.0.9.zip
+
+# Download Assimp version 4.1.0
+wget https://github.com/assimp/assimp/archive/v4.1.0.zip
+unzip assimp-v4.1.0.zip -d Assimp
+rm assimp-v4.1.0.zip
