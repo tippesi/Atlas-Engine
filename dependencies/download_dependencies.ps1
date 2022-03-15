d586d7dea0b3c0310054254ec5c37dc6f47c6c27
Simon Tippe <tippe.simon@gmail.com>
2022-03-15 16:40:48 +0100

    Updated Assimp and SDL dependencies

--- dependencies/download_dependencies.ps1
+++ dependencies/download_dependencies.ps1
@@ -13,7 +13,7 @@ rename-item "Imgui/imgui-1.87" "imgui"
 
 $file = "SDL.zip"
 
-Invoke-WebRequest -Uri "https://www.libsdl.org/release/SDL2-2.0.9.zip" -OutFile $file -TimeoutSec 120
+Invoke-WebRequest -Uri "https://www.libsdl.org/release/SDL2-2.0.20.zip" -OutFile $file -TimeoutSec 120
 
 # Unzip the file to specified location
 $location = Get-Location
@@ -21,11 +21,11 @@ $zip_file = (new-object -com shell.application).namespace("$location\$file")
 $destination = (new-object -com shell.application).namespace("$location")
 $items = $zip_file.items()
 $destination.Copyhere($items)
-rename-item "SDL2-2.0.9" "SDL"
+rename-item "SDL2-2.0.20" "SDL"
 
 $file = "Assimp.zip"
 
-Invoke-WebRequest -Uri "https://github.com/assimp/assimp/archive/v5.0.1.zip" -OutFile $file -TimeoutSec 120
+Invoke-WebRequest -Uri "https://github.com/assimp/assimp/archive/v5.2.2.zip" -OutFile $file -TimeoutSec 120
 
 # Unzip the file to specified location
 $location = Get-Location
@@ -33,7 +33,7 @@ $zip_file = (new-object -com shell.application).namespace("$location\$file")
 $destination = (new-object -com shell.application).namespace("$location")
 $items = $zip_file.items()
 $destination.Copyhere($items)
-rename-item "assimp-5.0.1" "Assimp"
+rename-item "assimp-5.2.2" "Assimp"
 
 remove-item SDL.zip
 remove-item Assimp.zip
--- dependencies/download_dependencies.sh
+++ dependencies/download_dependencies.sh
@@ -1,14 +1,14 @@
 # Download SDL2 version 2.0.9
-curl -O "https://www.libsdl.org/release/SDL2-2.0.9.zip"
-unzip SDL2-2.0.9.zip
-mv SDL2-2.0.9 SDL
-rm SDL2-2.0.9.zip
+curl -O "https://www.libsdl.org/release/SDL2-2.0.20.zip"
+unzip SDL2-2.0.20.zip
+mv SDL2-2.0.20 SDL
+rm SDL2-2.0.20.zip
 
 # Download Assimp version 5.0.1
-curl -O "https://codeload.github.com/assimp/assimp/zip/refs/tags/v5.0.1"
-unzip v5.0.1
-mv assimp-5.0.1 Assimp
-rm v5.0.1
+curl -O "https://codeload.github.com/assimp/assimp/zip/refs/tags/v5.2.2"
+unzip v5.2.2
+mv assimp-5.2.2 Assimp
+rm v5.2.2
 
 # Download Imgui version 1.87
 curl -O "https://codeload.github.com/ocornut/imgui/zip/refs/tags/v1.87"
