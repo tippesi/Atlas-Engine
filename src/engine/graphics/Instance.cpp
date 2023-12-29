dcd95342074e839a84b83ce42a44245d652dce8f
Simon Tippe <tippe.simon@gmail.com>
2023-12-29 19:05:08 +0100

    Try again

--- src/engine/graphics/Instance.cpp
+++ src/engine/graphics/Instance.cpp
@@ -227,10 +227,10 @@ namespace Atlas {
             VkDebugUtilsMessengerCreateInfoEXT createInfo{};
             createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
             createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
-            if (allowedSeverity >= 1) {
+            if (allowedSeverity <= 1) {
                 createInfo.messageSeverity |= VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT;
             }
-            if (allowedSeverity >= 0) {
+            if (allowedSeverity <= 0) {
                 createInfo.messageSeverity |= VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT |
                     VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT;
             }
