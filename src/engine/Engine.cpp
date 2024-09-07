0c21cc2fb3b5eb33933eb7d20183dfe9c7d1ec45
Simon Tippe <tippe.simon@gmail.com>
2024-09-07 21:05:15 +0200

    Turn validation layers off

--- src/engine/Engine.cpp
+++ src/engine/Engine.cpp
@@ -53,7 +53,7 @@ namespace Atlas {
 #ifdef AE_BUILDTYPE_DEBUG
             .enableValidationLayers = true,
 #else
-            .enableValidationLayers = true,
+            .enableValidationLayers = false,
 #endif
             .validationLayerSeverity = config.validationLayerSeverity,
 #ifndef AE_HEADLESS
